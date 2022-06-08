769bd053a25cadc3da25ba344d5481f4788ddc2c
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-06-08 20:40:11 +0530

    added Cloud-Gateway microservice

---
+++ could-gateway/src/main/java/com/CouldGatewayApplication.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CouldGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouldGatewayApplication.class, args);
+
+		System.out.println("CouldGateway Application running !");
+	}
+
+}
