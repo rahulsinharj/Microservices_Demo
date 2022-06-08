6f5cae59006e02c29220137df1575b4e78b127b0
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-06-08 17:53:27 +0530

    Microservices

--- service-registry/src/main/java/com/serviceReg/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/serviceReg/ServiceRegistryApplication.java
@@ -2,12 +2,16 @@ package com.serviceReg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
+
+		System.out.println("EurekaServer ServiceRegistry is running !");
 	}
 
 }
